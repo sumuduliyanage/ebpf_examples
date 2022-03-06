c8749967d1c238348ea561e0a6e20393b3baa50c
Sumudu Liyanage <62101884+sumuduliyanage@users.noreply.github.com>
2022-03-06 14:56:31 +0530

    Update hello.bpf.c

--- hello.bpf.c
+++ hello.bpf.c
@@ -9,14 +9,3 @@ int hello(void *ctx)
     return 0;
 }
 
-// Example of passing data using a perf map
-// Similar to bpftrace -e 'tracepoint:raw_syscalls:sys_enter { @[comm] = count();}'
-BPF_PERF_OUTPUT(events)
-SEC("raw_tracepoint/sys_enter")
-int hello_bpftrace(void *ctx)
-{
-    char data[100];
-    bpf_get_current_comm(&data, 100);
-    bpf_perf_event_output(ctx, &events, BPF_F_CURRENT_CPU, &data, 100);
-    return 0;
-}
